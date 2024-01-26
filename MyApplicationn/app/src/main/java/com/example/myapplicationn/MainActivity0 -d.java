340c884d0fe0162def88cdadfac1b7f9bbb377fd
Fidan Mahmudlu <126717367+fidanmahmudlu@users.noreply.github.com>
2024-01-26 14:05:52 +0400

    StyLab_edits

---
+++ MyApplicationn/app/src/main/java/com/example/myapplicationn/MainActivity0 -d.java
@@ -0,0 +1,40 @@
+package com.example.myapplicationn;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplicationn.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity0 extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @SuppressLint("NonConstantResourceId")
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main0);
+        setBottomNavigation();
+    }
+    public void setBottomNavigation(){
+        NavHostFragment navHostFragment=(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        assert navHostFragment!=null;
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+    }
+}
